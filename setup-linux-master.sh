ee34f23eb976ae5eb3f67d5e246603296e5456bf
Leah Hanson <lhanson@pivotal.io>
2018-02-27 10:48:34 -0500

    cleanup linux master create script
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- setup-linux-master.sh
+++ setup-linux-master.sh
@@ -3,12 +3,15 @@ scriptdir=$(dirname $0)
 masterVM=linux-master
 
 # Create the Linux master that will run the k8s control plane
+echo "creating linux master as vm name $masterVM ..."
 gcloud compute instances create "$masterVM" --image-project=ubuntu-os-cloud --image-family=ubuntu-1604-lts --zone=us-central1-c
+sleep 10
 gcloud compute scp "$scriptdir/run-this-on-linux-master.sh" "$masterVM:~/run-this-on-linux-master.sh"
 gcloud compute ssh "$masterVM" --command  'cd && ./run-this-on-linux-master.sh'
-gcloud compute scp "$masterVM:~/.kube/config" "/tmp/$masterVM-kube-config"
+echo "done!"
 
-# Create the windows node to host the pods
-#gcloud compute instances create new-windows-node --image-project=windows-cloud --image-family=windows-1709-core-for-containers --zone=us-central1-c
-
-
+# get config file for windows worker
+echo "downloading config file from master..."
+localconfig="/tmp/$masterVM-kube-config"
+gcloud compute scp "$masterVM:~/.kube/config" $localconfig
+echo "config file for master downloaded to: $localconfig"
